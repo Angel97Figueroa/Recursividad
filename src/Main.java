19c35ce404cb242b6a80cf47affd44ed3707617a
Val-0612 <diegovcg1@gmail.com>
2026-09-03 16:21:42 -0700

    Se agregaron los métodos de las orejas de conejo y las multiplicaciones con sumas sucesivas

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 public class Main{
     public static void main(){
-//        System.out.println(serieN0(5));
-//        System.out.println(serie0N(5));
+        System.out.println(serieN0(5));
+        System.out.println(serie0N(5));
         int nElementoDeFib=5;
         System.out.println(serieDeFibonacci(nElementoDeFib, 0, 1, nElementoDeFib));
+        int orejitasDeConejo= orejasDeConejo(17);
+        System.out.println(orejitasDeConejo);
+        System.out.println(multiplicar(5,10));
     }
 
     public static String serieN0(int n){
@@ -41,5 +44,24 @@ public class Main{
             return "0 1 "+fn+" "+serieDeFibonacci(n-1, fn2, fn1, nInicial);
         }
         return fn+" "+serieDeFibonacci(n-1, fn2, fn1, nInicial);
+    }
+
+    public static int orejasDeConejo(int n){
+        if(n==0){
+            return n;
+        }
+        else{
+            return orejasDeConejo(n-1)+ 2;
+        }
+    }
+
+    public static int multiplicar(int a, int b) {
+        if (b == 0) {
+            return 0;
+        } else if (b > 0) {
+            return a + multiplicar(a, b - 1);
+        } else {
+            return -a + multiplicar(a, b + 1);
+        }
     }
 }
