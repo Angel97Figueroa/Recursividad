77a3857257a21b8ad45cfacc0b33622a2dbc0fe6
Angel97Figueroa <244084261+Angel97Figueroa@users.noreply.github.com>
2026-09-03 14:34:04 -0700

    YA ESTA EL DE FIBONACCI

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main{
     public static void main(){
 //        System.out.println(serieN0(5));
 //        System.out.println(serie0N(5));
-        System.out.println(serieDeFibonacci(8, 0, 1));
+        System.out.println(serieDeFibonacci(8, 0, 1, 8));
     }
 
     public static String serieN0(int n){
@@ -19,15 +19,19 @@ public class Main{
         }
     }
 
-    public static String serieDeFibonacci(int n, int fn2, int fn1){
-        n--;
+    public static String serieDeFibonacci(int n, int fn2, int fn1, int m){
+        if(n==m){
+            n-=2;
+        }
         if(n==0){
-            return "0 1";
+            return "";
         }
         int fn=fn1+fn2;
         fn2=fn1;
         fn1=fn;
-        return fn+" "+serieDeFibonacci(n, fn2, fn1);
-
+        if(n==m-2){
+            return "0 1 "+fn+" "+serieDeFibonacci(n-1, fn2, fn1, m);
+        }
+        return fn+" "+serieDeFibonacci(n-1, fn2, fn1, m);
     }
 }
